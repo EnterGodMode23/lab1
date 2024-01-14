23a2074a551768ec3c86ab2d81ab20837c776c68
AveMaria23 <demyn300@gmail.com>
2024-01-14 16:15:16 +0300

    first commit

---
+++ lab1/ast.h
@@ -0,0 +1,18 @@
+#ifndef LAB1_AST_H
+#define LAB1_AST_H
+
+typedef struct ASTNode ASTNode;
+
+struct ASTNode {
+    char *type;
+    ASTNode *left;
+    ASTNode *right;
+    char *value;
+    int id;
+};
+
+void printAST();
+
+ASTNode *createNode(char *type, ASTNode *left, ASTNode *right, char *value);
+
+#endif //LAB1_AST_H
